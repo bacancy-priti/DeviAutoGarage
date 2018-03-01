40e084d1b3c0f1564b1772621ec082a71695ea0c
Priti Udiyar <priti.udiyar@bacancytechnology.com>
2018-03-01 16:14:41 +0530

    Newbundle

--- webAppDevi/App_Start/BundleConfig.cs
+++ webAppDevi/App_Start/BundleConfig.cs
@@ -11,22 +11,22 @@ namespace webAppDevi
             bundles.IgnoreList.Clear();
             //script
             bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
-                     "~/Scripts/bootstrap.js"
-           , "~/Scripts/jquery-1.8.3.js"
-     , "~/Scripts/jquery.validate.js"
-     , "~/Scripts/jquery.validate.unobtrusive.min.js"
-       , "~/Scripts/amcharts.js"
-        , "~/Scripts/serial.js"
-        , "~/Scripts/light.js"
-          , "~/Scripts/radar.js"
-           , "~/Scripts/vroom.js"
-              , "~/Scripts/TweenLite.min.js"
-                 , "~/Scripts/CSSPlugin.min.js"
-                    , "~/Scripts/jquery.nicescroll.js"
-                     , "~/Scripts/scripts.js"));
+                     "~/Scripts/bootstrap.js",
+                     "~/Scripts/jquery-1.8.3.js", 
+                     "~/Scripts/jquery.validate.js",
+                     "~/Scripts/jquery.validate.unobtrusive.min.js",
+                     "~/Scripts/amcharts.js",
+                     "~/Scripts/serial.js",         
+                     "~/Scripts/light.js",
+                     "~/Scripts/radar.js",
+                     "~/Scripts/vroom.js",                        
+                     "~/Scripts/TweenLite.min.js",
+                     "~/Scripts/CSSPlugin.min.js",
+                     "~/Scripts/jquery.nicescroll.js",
+                     "~/Scripts/scripts.js"));
             bundles.Add(new StyleBundle("~/bundles/css").Include(
-                "~/Content/css/bootstrap.min.css",
-                "~/Content/css/custom.css",
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/custom.css",
                       "~/Content/css/style.css",
                       "~/Content/css/font-awesome.css",
                       "~/Content/css/icon-font.min.css",
